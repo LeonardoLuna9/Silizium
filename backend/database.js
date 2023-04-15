6df1e4177751bc77476d9e3a808483208e9ad4c3
Wulf450 <drvmzo@hotmail.com>
2023-04-15 14:39:52 -0600

    First API functionalities

--- backend/database.js
+++ backend/database.js
@@ -1,9 +1,27 @@
-const mysql = require('mysql2')
-require('dotenv').config()
+const mysql = require('mysql2');
+require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
-}).promise()
+}).promise();
+
+async function getUsers() {
+    const [rows] = await pool.query("SELECT * FROM users");
+    return rows;
+}
+
+async function getUser(id) {
+    const [rows] = await pool.query(
+    `SELECT password 
+    FROM users
+    WHERE uid = ?`, [id]);
+    return rows[0];
+}
+
+getUsers().then(console.log);
+getUser('1234567890QW').then(console.log);
+
+module.exports = {getUser, getUsers};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const {getUser, getUsers} = require('./database.js');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
+
+app.get('/users', async (req,res) => {
+    res.send(await getUsers());
+});
 
 app.listen(port, () => {
     console.log('IBM server listening on port ' + port)
-})
+});
