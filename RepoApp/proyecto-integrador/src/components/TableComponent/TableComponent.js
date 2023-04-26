0ef2d56aaa1ea3436f07d0b64ba3e47cfa0092fa
LeonardoLuna9 <a01633305@itesm.mx>
2023-04-26 16:02:41 -0600

    TableComponent Beta

--- RepoApp/proyecto-integrador/src/components/TableComponent/TableComponent.js
+++ RepoApp/proyecto-integrador/src/components/TableComponent/TableComponent.js
@@ -1,13 +1,34 @@
 import "./TableComponent.css"
 
-const TableComponent = () => {
+import React from 'react';
 
-    return(
-        <div className="table-container">
-            <div>
-                
-            </div>
-        </div>
-    )
+const TableComponent = ({ data }) => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Column 1</th>
+          <th>Column 2</th>
+          <th>Column 3</th>
+          <th>Column 4</th>
+          <th>Column 5</th>
+          <th>Column 6</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            <td>{row.column1}</td>
+            <td>{row.column2}</td>
+            <td>{row.column3}</td>
+            <td>{row.column4}</td>
+            <td>{row.column5}</td>
+            <td>{row.column6}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
-}
+export default TableComponent;
