c06f6ffc5f13e2fe697254a0c132a74d319db560
PaulinaGtz <paulinagtz16@gmail.com>
2023-05-02 19:44:58 -0600

    Cambios Login

--- RepoApp/proyecto-integrador/src/pages/LoginPage.js
+++ RepoApp/proyecto-integrador/src/pages/LoginPage.js
@@ -5,17 +5,14 @@ import Login from '../components/Login/Login';
 
 const LoginPage = () => {
   return (
-    <div>
     <div className='login-container'>
-      <div className='left-side'>
-        <img src={image} alt="Login" />
-      </div>
-      <div className='right-side'>
-        <Login/>
-      </div>
-    </div>
-    <div className="rectangulo"> 
-    </div>
+        <div className='left-side'>
+          <img src={image} alt="Login" />
+        </div>
+        <div className='right-side'>
+          <Login/>
+        </div>
+      <div className="rectangulo"></div>
     </div>
   );
 };
